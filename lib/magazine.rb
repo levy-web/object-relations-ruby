4f0d777118b0ce77b99f309bcf3d0b3741588541
levy <lnjiru43@gmail.com>
2023-02-19 17:09:46 +0300

    added contributors and add by name methods

--- lib/magazine.rb
+++ lib/magazine.rb
@@ -20,4 +20,24 @@ class Magazine
         @category = String.new(category)
     end
 
+    def contributors
+        authors = Article.all
+        authors.collect do |author|
+            # author.author
+            if self.name == author.magazine.name
+                author.author
+            end
+        end
+    end
+
+    def self.find_by_name(name)
+        magazines = Magazine.all
+        magazines.find do |magazine|
+            if name == magazine.name
+                magazine
+            end
+            
+        end
+    end
+
 end
